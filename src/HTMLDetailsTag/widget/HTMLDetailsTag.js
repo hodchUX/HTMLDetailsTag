52a4b719646d580dea330171fbbaeb12b5df5937
Christopher James Hodges <chris.hodges@mendix.com>
2017-02-01 13:27:42 +0000

    Initial Project Upload

---
+++ src/HTMLDetailsTag/widget/HTMLDetailsTag.js
@@ -0,0 +1,100 @@
+/*global logger*/
+/*
+    HTMLDetailsTag
+    ========================
+
+    @file      : HTMLDetailsTag.js
+    @version   : 1.0.0
+    @author    : Christopher James Hodges
+    @date      : 2/1/2017
+    @copyright : Mendix Technology Ltd
+    @license   : Apache 2
+
+    Documentation
+    ========================
+    Describe your widget here.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+
+    "HTMLDetailsTag/lib/jquery-1.11.2",
+    "dojo/text!HTMLDetailsTag/widget/template/HTMLDetailsTag.html"
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, _jQuery, widgetTemplate) {
+    "use strict";
+
+    var $ = _jQuery.noConflict(true);
+
+    // Declare widget's prototype.
+    return declare("HTMLDetailsTag.widget.HTMLDetailsTag", [ _WidgetBase, _TemplatedMixin ], {
+        // _TemplatedMixin will create our dom node using this HTML template.
+        templateString: widgetTemplate,
+
+        // DOM elements
+        headerNode: null,
+        contentNode: null,
+
+        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+        constructor: function () {
+            logger.debug(this.id + ".constructor");
+        },
+
+        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+        postCreate: function () {
+            logger.debug(this.id + ".postCreate");
+
+            this._setupEvents();
+        },
+
+        // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
+        enable: function () {
+          logger.debug(this.id + ".enable");
+        },
+
+        // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
+        disable: function () {
+          logger.debug(this.id + ".disable");
+        },
+
+        // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
+        resize: function (box) {
+          logger.debug(this.id + ".resize");
+        },
+
+        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+        uninitialize: function () {
+          logger.debug(this.id + ".uninitialize");
+            // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+        },
+
+        // Attach events to HTML dom elements
+        _setupEvents: function () {
+            logger.debug(this.id + "._setupEvents");
+            
+            dojoHtml.set(this.headerNode, this.header);
+            dojoHtml.set(this.contentNode, this.contents);
+            
+            if (this.applyClass !== ""){
+                 dojo.addClass(this.contentNode, this.applyClass);
+            }
+        }
+    });
+});
+
+require(["HTMLDetailsTag/widget/HTMLDetailsTag"]);
